6f63fa645123bd807a0002cff1e7488bb8279170
Brice Webster <bwebster@vpchc.org>
2018-06-12 17:00:05 -0400

    Locations Partiallly Done
    *Location Index html changed to reflect style and refactor changes.
    *Maps css moved in vendor section and now compiles into the new overall css sheet.
    *Location page styles mostly moved from main css into page-specific sassle file. Working on mobile styles now.
    *Bloomingdale html changed to reflect style changes and refactoring. Still need the other locations.
    *Created centered small & large sub style for centered content.
    *H4 tag is used for locations title.

--- bloomingdale/index.php
+++ bloomingdale/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_2.0.css" rel="stylesheet" type="text/css" />
+<link href="/css/apps.css" rel="stylesheet" type="text/css" />
 <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
@@ -41,60 +41,58 @@
 </head>
 <body>
 <header><?php include("../php/includes/header.php"); ?></header>
-<main id="masterbox">
-  <p id="location-title">Bloomingdale</p>
-  <div id="location-top-section">
-    <img id="location-photo" src="/images/locations/banner_bloomingdale.jpg"/>
-    <div id="location-info">
-      <p class="location-subtitle">CONTACT US</p>
+<main>
+	<div class="content centered large">
+  <h4>Bloomingdale</h4>
+  <img class="location-photo" src="/images/locations/banner_bloomingdale.jpg"/>
+  <div class="location-info">
+      <p class="location-title">CONTACT US</p>
       <p>201 W Academy Street<br/>Bloomingdale, IN 47842</p>
-      <div class="location-info-contact">
-        <img class="location-info-icon" src="/images/locations/icon_phone.png" alt="phone number">
+      <div class="location-contact">
+        <img src="/images/locations/icon_phone.png" alt="phone number">
         <p>(765) 498-9000</p>
       </div>
-      <div class="location-info-contact">
-        <img class="location-info-icon" src="/images/locations/icon_fax.png" alt="fax number">
+      <div class="location-contact">
+        <img src="/images/locations/icon_fax.png" alt="fax number">
         <p>(765) 498-9004</p>
       </div>
-      <p class="location-subtitle">HOURS</p>
+      <p class="location-title">HOURS</p>
       <p>Monday - Friday<br/>8:00 am to 5:00 pm</p>
       <p>Closed<br/>12:00 pm to 1:00 pm</p>
-      <p class="location-tinytitle">Extended Hours</p>
+      <p class="location-subtitle">Extended Hours</p>
       <p>Monday<br/>5:00 pm to 8:00 pm</p>
-    </div>
   </div>
-  <div id="location-bottom-section">
-    <div id="location-details">
-      <p class="location-subtitle location-subtitle-topmargin">SERVICES</p>
-      <div class="location-services-type">
-        <a href="/services/primary_care.php"><img class="location-services-icon" src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
-        <span>Primary Care</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/services/behavioral_health.php"><img class="location-services-icon" src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
-        <span>Behavioral Health</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/services/patient_support.php"><img class="location-services-icon" src="/images/services/ps_icon.png" alt="Patient Support services"/></a>
-        <span>Patient Support</span>
-      </div>
-      <p class="location-subtitle location-subtitle-topmargin">PROVIDERS</p>
-      <table class="team-table">
-        <tr>
-          <td><p class="team-table-name">Dr. Christopher Fitzsimmons, MD</p></td> 
-          <td><p class="team-table-name">Christi Busenbark, FNP-C</p></td> 
-        </tr>
-        <tr>
-          <td><p class="team-table-name">Louwanna Wallace, FNP-C</p></td>
-					<td><p class="team-table-name">Zac Milam, LMHC</p></td>
-        </tr>
-        <tr>
-          <td><p class="team-table-name">Megan Neitling, LMHCA</p></td>
-        </tr>
-      </table>
+  <div class="location-details">
+  	<p class="location-title">SERVICES</p>
+    <div class="location-services">
+      <a href="/services/primary_care.php"><img src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
+      <span>Primary Care</span>
     </div>
-    <iframe id="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3063.953823364543!2d-87.25627278497387!3d39.83045297943764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886d49add4238a7f%3A0x51cc38f528ac8d85!2sValley+Professionals+Community+Health+Center!5e0!3m2!1sen!2sus!4v1505917741280" allowfullscreen></iframe>
+    <div class="location-services">
+      <a href="/services/behavioral_health.php"><img src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
+      <span>Behavioral Health</span>
+    </div>
+    <div class="location-services">
+    	<a href="/services/patient_support.php"><img src="/images/services/ps_icon.png" alt="Patient Support services"/></a>
+      <span>Patient Support</span>
+    </div>
+		<p class="location-title">PROVIDERS</p>
+    <table class="team-table">
+    <tr>
+      <td><p>Dr. Christopher Fitzsimmons, MD</p></td> 
+      <td><p>Christi Busenbark, FNP-C</p></td> 
+    </tr>
+    <tr>
+			<td><p>Louwanna Wallace, FNP-C</p></td>
+			<td><p>Zac Milam, LMHC</p></td>     
+		</tr>
+    <tr>
+			<td><p>Megan Neitling, LMHCA</p></td>
+    </tr>
+		</table>
   </div>
+  <iframe class="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3063.953823364543!2d-87.25627278497387!3d39.83045297943764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886d49add4238a7f%3A0x51cc38f528ac8d85!2sValley+Professionals+Community+Health+Center!5e0!3m2!1sen!2sus!4v1505917741280" allowfullscreen></iframe>
+	</div>
 </main>
 <footer><?php include("../php/includes/footer.php"); ?></footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
