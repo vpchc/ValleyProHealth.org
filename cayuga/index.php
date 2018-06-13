85e01bd45a5ea981644890759cf5d9ad0e837dd1
Brice Webster <bwebster@vpchc.org>
2018-06-13 14:19:56 -0400

    Location Pages Moved
    *Location pages css all moved from main css into page specific sass file.
    *Cayuga, Clinton, Crawfordsville, Terre Haute and Mobile html changed to reflect style changes and refactoring.
    *Bus Tracker simplified to spans to better look and easier to maintain.

--- cayuga/index.php
+++ cayuga/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_2.0.css" rel="stylesheet" type="text/css" />
+<link href="/css/apps.css" rel="stylesheet" type="text/css" />
 <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
@@ -41,68 +41,66 @@
 </head>
 <body>
 <header><?php include("../php/includes/header.php"); ?></header>
-<main id="masterbox">
-  <p id="location-title">Cayuga</p>
-  <div id="location-top-section">
-    <img id="location-photo" src="/images/locations/banner_cayuga.jpg"/>
-    <div id="location-info">
-      <p class="location-subtitle">CONTACT US</p>
-      <p>703 W. Park Street<br/>Cayuga, IN 47928</p>
-      <div class="location-info-contact">
-        <img class="location-info-icon" src="/images/locations/icon_phone.png" alt="phone number">
-        <p>(765) 492-9042</p>
-      </div>
-      <div class="location-info-contact">
-        <img class="location-info-icon" src="/images/locations/icon_fax.png" alt="fax number">
-        <p>(765) 492-9048</p>
-      </div>
-      <p class="location-subtitle">HOURS</p>
-      <p>Monday - Friday<br/>8:00 am to 5:00 pm</p>
-      <p>Closed<br/>12:00 pm to 1:00 pm</p>
-      <p class="location-tinytitle">Extended Hours</p>
-      <p>Wednesday<br/>5:00 pm to 8:00 pm</p>
+<main>
+	<div class="content centered large">
+  <h4>Cayuga</h4>
+	<img class="location-photo" src="/images/locations/banner_cayuga.jpg"/>
+  <div class="location-info">
+  	<p class="location-title">CONTACT US</p>
+    <p>703 W. Park Street<br/>Cayuga, IN 47928</p>
+    <div class="location-contact">
+    	<img src="/images/locations/icon_phone.png" alt="phone number">
+      <p>(765) 492-9042</p>
     </div>
+    <div class="location-contact">
+    	<img src="/images/locations/icon_fax.png" alt="fax number">
+      <p>(765) 492-9048</p>
+    </div>
+    <p class="location-title">HOURS</p>
+    <p>Monday - Friday<br/>8:00 am to 5:00 pm</p>
+    <p>Closed<br/>12:00 pm to 1:00 pm</p>
+    <p class="location-subtitle">Extended Hours</p>
+    <p>Wednesday<br/>5:00 pm to 8:00 pm</p>
   </div>
-  <div id="location-bottom-section">
-    <div id="location-details">
-      <p class="location-subtitle location-subtitle-topmargin">SERVICES</p>
-      <div class="location-services-type">
-        <a href="/services/primary_care.php"><img class="location-services-icon" src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
-        <span>Primary Care</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/services/behavioral_health.php"><img class="location-services-icon" src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
-        <span>Behavioral Health</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/services/patient_support.php"><img class="location-services-icon" src="/images/services/ps_icon.png" alt="Patient Support services"/></a>
-        <span>Patient Support</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/dental/"><img class="location-services-icon" src="/images/services/dent_icon.png" alt="Dental services"/></a>
-        <span>Dental</span>
-      </div>
-      <p class="location-subtitle location-subtitle-topmargin">PROVIDERS</p>
-      <table class="team-table">
-        <tr>
-					<td><p class="team-table-name">Dr. Danielle Cundiff, DO</p></td>
-          <td><p class="team-table-name">Dr. Bing Gale, MD</p></td> 
-        </tr>
-        <tr>
-          <td><p class="team-table-name">Renae Norman, FNP-C</p></td>
-          <td><p class="team-table-name">Dr. Paul Taraska, MD</p></td>
-        </tr>
-        <tr>
-          <td><p class="team-table-name">Johnathan Detwiler, MS</p></td>
-          <td><p class="team-table-name">Tasha Lewis-Stevens, LCSW, RN</p></td>
-        </tr>
-        <tr>
-          <td><p class="team-table-name">Lisa Tincher, PMHNP</p></td>
-          <td><p class="team-table-name">Dr. Nichole Barnett, DDS</p></td>
-        </tr>
-      </table>
+  <div class="location-details">
+  	<p class="location-title">SERVICES</p>
+    <div class="location-services">
+    	<a href="/services/primary_care.php"><img src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
+      <span>Primary Care</span>
     </div>
-    <iframe id="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.0347672384405!2d-87.47126568497!3d39.94061197942334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886d54aa655d565f%3A0x3e0ddae5b4218ab7!2sValley+Professionals+Community+Health+Center!5e0!3m2!1sen!2sus!4v1505917855515" allowfullscreen></iframe>
+    <div class="location-services">
+    	<a href="/services/behavioral_health.php"><img src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
+      <span>Behavioral Health</span>
+    </div>
+    <div class="location-services">
+    	<a href="/services/patient_support.php"><img src="/images/services/ps_icon.png" alt="Patient Support services"/></a>
+      <span>Patient Support</span>
+    </div>
+    <div class="location-services">
+    	<a href="/dental/"><img src="/images/services/dent_icon.png" alt="Dental services"/></a>
+      <span>Dental</span>
+    </div>
+    <p class="location-title">PROVIDERS</p>
+    <table class="team-table">
+    	<tr>
+				<td><p class="team-table-name">Dr. Danielle Cundiff, DO</p></td>
+        <td><p class="team-table-name">Dr. Bing Gale, MD</p></td> 
+      </tr>
+      <tr>
+      	<td><p class="team-table-name">Renae Norman, FNP-C</p></td>
+        <td><p class="team-table-name">Dr. Paul Taraska, MD</p></td>
+      </tr>
+      <tr>
+      	<td><p class="team-table-name">Johnathan Detwiler, MS</p></td>
+        <td><p class="team-table-name">Tasha Lewis-Stevens, LCSW, RN</p></td>
+      </tr>
+      <tr>
+      	<td><p class="team-table-name">Lisa Tincher, PMHNP</p></td>
+        <td><p class="team-table-name">Dr. Nichole Barnett, DDS</p></td>
+    	</tr>
+    </table>
+  </div>
+  <iframe class="location-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3059.0347672384405!2d-87.47126568497!3d39.94061197942334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886d54aa655d565f%3A0x3e0ddae5b4218ab7!2sValley+Professionals+Community+Health+Center!5e0!3m2!1sen!2sus!4v1505917855515" allowfullscreen></iframe>
   </div>
 </main>
 <footer><?php include("../php/includes/footer.php"); ?></footer>
--- mobile/index.php
+++ mobile/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_2.0.css" rel="stylesheet" type="text/css" />
+<link href="/css/apps.css" rel="stylesheet" type="text/css" />
 <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
@@ -41,64 +41,48 @@
 </head>
 <body>
 <header><?php include("../php/includes/header.php"); ?></header>
-<main id="masterbox">
-  <p id="location-title">Mobile School-Based Health Center</p>
-  <div id="location-top-section">
-    <img id="location-photo" src="/images/locations/banner_msbhc.jpg"/>
-    <div id="location-info">
-      <p class="location-subtitle">CONTACT US</p>
-      <div class="location-info-contact">
-        <img class="location-info-icon" src="/images/locations/icon_phone.png" alt="phone number">
-        <p>(765) 592-6164</p>
-        <p>mobile@vpchc.org</p>
-      </div>
+<main>
+  <div class="content centered large">
+	<h4>Mobile School-Based Health Center</h4>
+  <img class="location-photo" src="/images/locations/banner_msbhc.jpg"/>
+  <div class="location-info">
+  	<p class="location-title">CONTACT US</p>
+    <div class="location-contact">
+    	<img src="/images/locations/icon_phone.png" alt="phone number">
+      <p>(765) 592-6164</p>
+      <p>mobile@vpchc.org</p>
     </div>
   </div>
-  <div id="location-bottom-section">
-    <div id="location-details">
-      <p class="location-subtitle location-subtitle-topmargin">SERVICES</p>
-      <div class="location-services-type">
-        <a href="/services/primary_care.php"><img class="location-services-icon" src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
-        <span>Primary Care</span>
-      </div>
-      <div class="location-services-type">
-        <a href="/services/behavioral_health.php"><img class="location-services-icon" src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
-        <span>Behavioral Health</span>
-      </div>
-      <p class="location-subtitle location-subtitle-topmargin">PROVIDERS</p>
-      <table class="team-table">
-      <tr> 
-        <td><p class="team-table-name">Nicole Cook, FNP-C</p></td> 
-        <td><p class="team-table-name">Renae Norman, FNP-C</p></td>
+  <div class="location-details">
+  	<p class="location-title">SERVICES</p>
+  	<div class="location-services">
+  		<a href="/services/primary_care.php"><img src="/images/services/med_icon.png"/ alt="Primary Care services"></a>
+     	<span>Primary Care</span>
+    </div>
+    <div class="location-services">
+    	<a href="/services/behavioral_health.php"><img src="/images/services/bh_icon.png" alt="Behavioral Health services"/></a>
+      <span>Behavioral Health</span>
+    </div>
+    <p class="location-title">PROVIDERS</p>
+    <table class="team-table">
+    	<tr> 
+      	<td><p>Nicole Cook, FNP-C</p></td> 
+        <td><p>Renae Norman, FNP-C</p></td>
       </tr>
       <tr>
-        <td><p class="team-table-name">Heather Woods, LMHC</p></td>
+        <td><p>Heather Woods, LMHC</p></td>
       </tr>
     </table>
+  </div>
+  <div class="bus-tracker">
+  	<div class="bus-tracker-title">Where is the </br> Mobile Health Center?</div>
+    <div class="bus-tracker-info">
+    	<p>Location:<span id="bus-location" class="bus-tracker-data"></span></p>
+      <p>Hours:<span id="bus-hours" class="bus-tracker-data"></span></p>
+      <p>Status:<span class="bus-tracker-data"><img id="bus-status-icon" src="" alt="Status Icon"/><span id="bus-status"></span></span></p>
     </div>
-    <div id="location-map">
-    <div id="bus-tracker" class="banner-section-left">
-        <div id="bus-tracker-title">Where is the </br> Mobile Health Center?</div>
-        <div id="bus-tracker-info">
-          <p>Location:</p>
-          <p>Hours:</p>
-          <p>Status:</p>
-        </div>
-        <div id="bus-tracker-data">
-          <p id="bus-location" class="bus-tracker-data-section"></p>
-          <p id="bus-hours" class="bus-tracker-data-section"></p>
-          <div id="bus-tracker-data-status-section">
-            <div id="bus-tracker-data-status-section-sub">
-              <img id="bus-status-icon" src="" alt="Status Icon"/>
-              <p id="bus-status" class="bus-tracker-data-section-status"></p>
-            </div>
-          </div>
-        </div>
-        <div id="bus-tracker-view">
-          <a class="vpchcbutton" href="https://calendar.google.com/calendar/embed?src=flc9fq7tt9nlo52qhlchoscu3s%40group.calendar.google.com&ctz=America/New_York">View Schedule</a>
-        </div>
-    </div>
-    </div>
+    <a class="bus-tracker-view vpchcbutton" href="https://calendar.google.com/calendar/embed?src=flc9fq7tt9nlo52qhlchoscu3s%40group.calendar.google.com&ctz=America/New_York">View Schedule</a>
+  </div>
   </div>
 </main>
 <footer><?php include("../php/includes/footer.php"); ?></footer>
