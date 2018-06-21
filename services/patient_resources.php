64bb4565b5e9d9d31c041ee3736573a987255f46
Brice Webster <bwebster@vpchc.org>
2018-06-21 17:03:07 -0400

    Patient Resources Page Moved
    *Patien Resources html changed to reflect style changes and refactoring.
    *Content li style added to global sass file.

--- services/patient_resources.php
+++ services/patient_resources.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_2.0.css" rel="stylesheet" type="text/css" />
+<link href="/css/apps.css" rel="stylesheet" type="text/css" />
 <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
 <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
@@ -41,69 +41,40 @@
 </head>
 <body>
 <header><?php include("../php/includes/header.php"); ?></header>
-<main id="masterbox-nonfixed">
-  <div id="banner">
+<main>
+  <div class="banner">
     <img src="/images/banner/services_patient_resources.jpg">
-    <div id="banner-title">
-      <p>Patient Resources</p>
+    <div class="banner-title">
+      <h1>Patient Resources</h1>
     </div>
   </div>
-  <div id="explain-content">
-    <p class="explain-title vpchcgreen">Where Healthy Happens</p>
-    <p class="explain-p">Valley Professionals is here to lend a helping hand in keeping our communities happy and healthy.  Check out the links below to explore some of the resources that Valley Professionals and our community partners have to offer. Click each title to see more.</p>
-    <p id="patres-scale-title" class="explain-title vpchcgreen">Sliding Fee Scale</p>
-    <div id="patres-scale">
-    <p class="explain-p">If a patient is uninsured or underinsured, a sliding fee scale is available for patients to receive a discounted rate. Discounts available are based on patient family size and income in accordance with the most recent U.S. Department of Health & Human Services Federal Poverty Guidelines.</br></br>The sliding fee scale is based upon the current income of all family members, and the number of people in the household. An individual who is unemployed, has no income and is being supported by others must provide documentation showing that no federal income taxes have been filed, and the person supporting the patient must provide written and signed documentation that they are providing food and shelter for the patient. </br></br> In order to qualify for the sliding fee scale at VPCHC, patients and their families must have a combined income that falls within the U.S. Federal Poverty Guidelines.</p>
-    </div>
-    <p id="patres-script-title" class="explain-title vpchcgreen">Prescription Assistance</p>
-    <div id="patres-script">
-    <p class="explain-p">We have several ways to help patients that are having trouble affording their medication.</br></br><b>340B Program:</b> This program helps cover the cost of many precscription medications. Based on your family income and size determines how much you have to pay for your medications. Application for the sliding fee scale is required for this program. If you qualify, medications may be free or low cost.</br></br><b>Drug Companies:</b> Many of commonly prescribed drugs can be obtained through drug companies at no cost or low cost. Our Community Health Workers can assist in these applications.</br></br><b>Good Rx:</b> The website <a href="http://www.goodrx.com">goodrx.com</a> has comparison shopping on prescriptions and offer many good coupons.</br></br> Additional programs are also available such as discount drug cards and the Freestyle Promise Program.</p>
-    </div>
-    <p id="patres-diabetes-title" class="explain-title vpchcgreen">Diabetic Support Group</p>
-    <div id="patres-diabetes">
-    <p class="explain-p">Valley Professionals holds monthly Diabetes Support Group meetings that are completely free and open to anyone who is affected by Diabetes, even if you are not a patient with Valley Professionals. For more information, click <a href="/diabetes/">here</a>.</p>
-    </div>
-    <p id="patres-bike-title" class="explain-title vpchcgreen">Bike Program</p>
-    <div id="patres-bike">
-    <p class="explain-p">Valley Professionals rewards kids who care about their health.</br>Call and schedule a well-child exam for your child at any of our Valley Professionals clinics and he or she could win a brand-new bike!</br>Patients who come in for a well-child visit and are up-to-date on their vaccinations will be entered into the drawing during the month of their visit. One winner is drawn every month.</br>Good luck!</p>
-    </div>
-    <p id="patres-links-title" class="explain-title vpchcgreen">Links to Community Partners</p>
-    <div id="patres-links">
-    <p class="explain-p">For assistance applying for Health insurance and/or finding community resources.</br><li>VPCHC Community Health Workers are located at 5 of our locations. If you qualify, you may be eligible for Presumptive Eligibility also known as same day health coverage!</li></p>
-    <p class="explain-p">Enroll America Insurance Information</br><li><a href="http://www.enrollamerica.org">enrollamerica.org</a></li></p></br>
-    <p class="explain-p">To apply for HIP, Medicaid, and/or SNAP Benefits</br><li>FSSA Office</br>1-800-403-0864</br><a href="https://fssabenefits.in.gov">FSSA Benefits</a></li></p></br>
-    <p class="explain-p">To browse marketplace coverage and/or apply</br><li><a href="https://www.healthcare.gov">healthcare.gov</a></li></p></br>
-    <p class="explain-p">2-1-1 Various Available Resources</br><li><a href="http://www.nwi211.com/Search.aspx">nwi211</a></li></p></br>
-    <p class="explain-p">Community Action Local Resources</br><li><a href="http://www.capwi.org">capwi.org</a></li></p></br>
-    <p class="explain-p">Area 7 Agency on Aging</br><li><a href="http://www.westcentralin.com">westcentralin.com</a></li></p></br>
-    <p class="explain-p">Medicare</br><li><a href="https://medicare.gov/findaplan">medicare.gov</a></li></p></br>
-    <p class="explain-p">Social Security</br><li><a href="https://www.ssa.gov">ssa.gov</a></li></p></br>
-    <p class="explain-p">GoodRx Prescription Discounts</br><li><a href="https://www.goodrx.com/">goodrx.com</a></li></p></br>
-    <p class="explain-p">Voter Registration for the state of Indiana</br><li><a href="https://indianavoters.in.gov/PublicSite/OVR/Introduction.aspx?Link=Polling">indianavoters.in.gov</a></li></p></br>
-    </div>
+  <div class="content">
+    <h2>Where Healthy Happens</h2>
+		<p>Valley Professionals is here to lend a helping hand in keeping our communities happy and healthy.  Check out the links below to explore some of the resources that Valley Professionals and our community partners have to offer.</p>
+    <h2>Sliding Fee Scale</h2>
+    <p>If a patient is uninsured or underinsured, a sliding fee scale is available for patients to receive a discounted rate. Discounts available are based on patient family size and income in accordance with the most recent U.S. Department of Health & Human Services Federal Poverty Guidelines.</br></br>The sliding fee scale is based upon the current income of all family members, and the number of people in the household. An individual who is unemployed, has no income and is being supported by others must provide documentation showing that no federal income taxes have been filed, and the person supporting the patient must provide written and signed documentation that they are providing food and shelter for the patient. </br></br> In order to qualify for the sliding fee scale at VPCHC, patients and their families must have a combined income that falls within the U.S. Federal Poverty Guidelines.</p>
+    <h2>Prescription Assistance</h2>
+    <p>We have several ways to help patients that are having trouble affording their medication.</br></br><b>340B Program:</b> This program helps cover the cost of many precscription medications. Based on your family income and size determines how much you have to pay for your medications. Application for the sliding fee scale is required for this program. If you qualify, medications may be free or low cost.</br></br><b>Drug Companies:</b> Many of commonly prescribed drugs can be obtained through drug companies at no cost or low cost. Our Community Health Workers can assist in these applications.</br></br><b>Good Rx:</b> The website <a href="http://www.goodrx.com">goodrx.com</a> has comparison shopping on prescriptions and offer many good coupons.</br></br> Additional programs are also available such as discount drug cards and the Freestyle Promise Program.</p>
+    <h2>Diabetic Support Group</h2>
+    <p>Valley Professionals holds monthly Diabetes Support Group meetings that are completely free and open to anyone who is affected by Diabetes, even if you are not a patient with Valley Professionals. For more information, click <a href="/diabetes/">here</a>.</p>
+    <h2>Bike Program</h2>
+    <p>Valley Professionals rewards kids who care about their health.</br>Call and schedule a well-child exam for your child at any of our Valley Professionals clinics and he or she could win a brand-new bike!</br>Patients who come in for a well-child visit and are up-to-date on their vaccinations will be entered into the drawing during the month of their visit. One winner is drawn every month.</br>Good luck!</p>
+    <h2>Links to Community Partners</h2>
+    <p>For assistance applying for Health insurance and/or finding community resources.</br><li>VPCHC Community Health Workers are located at 5 of our locations. If you qualify, you may be eligible for Presumptive Eligibility also known as same day health coverage!</li></p>
+    <p>Enroll America Insurance Information</br><li><a href="http://www.enrollamerica.org">enrollamerica.org</a></li></p></br>
+    <p>To apply for HIP, Medicaid, and/or SNAP Benefits</br><li>FSSA Office</br>1-800-403-0864</br><a href="https://fssabenefits.in.gov">FSSA Benefits</a></li></p></br>
+    <p>To browse marketplace coverage and/or apply</br><li><a href="https://www.healthcare.gov">healthcare.gov</a></li></p></br>
+    <p>2-1-1 Various Available Resources</br><li><a href="http://www.nwi211.com/Search.aspx">nwi211</a></li></p></br>
+    <p>Community Action Local Resources</br><li><a href="http://www.capwi.org">capwi.org</a></li></p></br>
+    <p>Area 7 Agency on Aging</br><li><a href="http://www.westcentralin.com">westcentralin.com</a></li></p></br>
+    <p>Medicare</br><li><a href="https://medicare.gov/findaplan">medicare.gov</a></li></p></br>
+    <p>Social Security</br><li><a href="https://www.ssa.gov">ssa.gov</a></li></p></br>
+    <p>GoodRx Prescription Discounts</br><li><a href="https://www.goodrx.com/">goodrx.com</a></li></p></br>
+    <p>Voter Registration for the state of Indiana</br><li><a href="https://indianavoters.in.gov/PublicSite/OVR/Introduction.aspx?Link=Polling">indianavoters.in.gov</a></li></p></br>
   </div>
 </main>
 <footer><?php include("../php/includes/footer.php"); ?></footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="/scripts/vpchc_mobile_menu.js"></script>
-<script>
-$(document).ready(function(){
-   $("#patres-scale-title").click(function(){
-       $("#patres-scale").slideToggle("slow");
-    });
-   $("#patres-script-title").click(function(){
-      $("#patres-script").slideToggle("slow");
-    });
-   $("#patres-diabetes-title").click(function(){
-       $("#patres-diabetes").slideToggle("slow");
-    });
-   $("#patres-bike-title").click(function(){
-      $("#patres-bike").slideToggle("slow");
-    });
-   $("#patres-links-title").click(function(){
-       $("#patres-links").slideToggle("slow");
-    });
-});
-</script>
 </body>
 </html>
