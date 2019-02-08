8b8cfad0ef6ec31edb0df777f610e4fdb120914c
Bill Oellig <boellig@uhhg.org>
2019-02-08 15:13:29 -0500

    Removed two job postings
    Removed two CHW positions from careers page

--- careers/index.php
+++ careers/index.php
@@ -82,17 +82,9 @@
     </div>
 		<h2 id="jobopenings">Job Openings</h2>
     <p>Interested in a career with Valley Professionals?</p>
-    <p>View current job postings and submit your resume below.</p>
-		<div class="content-tiles">
-			<a href="/careers/job_listing_2.php"><div class="content-tile">
-			<p>Community Health Worker</p>
-			<img src="/images/careers/location_pointer.png"/><p>Terre Haute</p>
-      </div></a>
-			<a href="/careers/job_listing_3.php"><div class="content-tile">
-			<p>Community Health Worker - Float</p>
-			<img src="/images/careers/location_pointer.png"/><p>Clinton</p>
-      </div></a>
-            <a href="/careers/job_listing_4.php"><div class="content-tile">
+<p>View current job postings and submit your resume below.</p> </br>
+    <div>
+		    <a href="/careers/job_listing_4.php"><div class="content-tile">
 			<p>Psychologist</p>
 			<img src="/images/careers/location_pointer.png"/><p>Rockville</p>
       </div></a>
