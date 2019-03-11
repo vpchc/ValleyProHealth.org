d95da1f22247c8498be1f12e18845094630d17ec
Elizabeth <elizabeth@itindianapolis.com>
2019-03-11 09:44:26 -0400

    Changes - Round 1 - Going Live

--- contact_us/index.php
+++ contact_us/index.php
@@ -1,128 +1,128 @@
-<!doctype html>
-<html lang="en">
-<head>
-<title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles.css" rel="stylesheet" type="text/css" />
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//valleyprohealth.org/stats/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-128496723-1');
-</script>
-<!--.....|.........|.........|.........|.........|.........|.........|.......-->
-</head>
-<body>
-<header><?php include("../php/includes/header.php"); ?></header>
-<main>
-  <div class="banner">
-    <img src="/images/banner/contact_us_index.jpg">
-    <div class="banner-title">
-      <h1>Contact Us</h1>
-    </div>
-  </div>
-  <div class="content centered medium">
-		<table class="contact-table">
-			<tr>
-				<td>
-				 <a href="/bloomingdale/"><img src="/images/locations_small/bloomingdale.jpg" alt="Bloomingdale"/></a>
-				 <p class="contact-location">Bloomingdale</p>
-				 <p class="contact-info street">201 W Academy Street</p>
-				 <p class="contact-info state">Bloomingdale, IN 47832</p>
-				 <p class="contact-info">(765) 498-9000</p>
-				</td>
-				<td>
-				 <a href="/cayuga/"><img src="/images/locations_small/cayuga.jpg" alt="Cayuga"/></a>
-				 <p class="contact-location">Cayuga</p>
-				 <p class="contact-info street">703 W Park Street</p>
-				 <p class="contact-info state">Cayuga, IN 47928</p>
-				 <p class="contact-info">(765) 498-9042</p>
-				</td> 
-				<td>
-				 <a href="/clinton/"><img src="/images/locations_small/clinton.jpg" alt="Clinton"/></a>
-				 <p class="contact-location">Clinton</p>
-				 <p class="contact-info street">777 S Main Street, #100</p>
-				 <p class="contact-info state">Clinton, IN 47842</p>
-				 <p class="contact-info">(765) 828-1003</p>
-				</td> 
-			</tr>
-			<tr>
-				<td>
-				 <a href="/crawfordsville/"><img src="/images/locations_small/banner_crawfordsville3.jpg" alt="Crawfordsville"/></a>
-				 <p class="contact-location">Crawfordsville</p>
-				 <p class="contact-info street">1702 Lafayette Road</p>
-				 <p class="contact-info state">Crawfordsville, IN 47933</p>
-				 <p class="contact-info">(765) 362-5100</p>
-				</td>
-				<td>
-				 <a href="/rockville/"><img src="/images/locations_small/rockville.jpg" alt="Rockville"/></a>
-				 <p class="contact-location">Rockville</p>
-				 <p class="contact-info street">727 Lincoln Rd</p>
-				 <p class="contact-info state">Rockville, IN 47842</p>
-				 <p class="contact-info">(765) 569-1123</p>
-				</td> 
-				<td>
-				 <a href="/terrehaute"><img src="/images/locations_small/terrehaute.jpg" alt="Terre Haute"/></a>
-				 <p class="contact-location">Terre Haute</p>
-				 <p class="contact-info street">1530 N 7th Street, #201</p>
-				 <p class="contact-info state">Terre Haute, IN 47807</p>
-				 <p class="contact-info">(812) 238-7631</p>
-				</td> 
-			</tr>
-			<tr>
-				<td class="contact-hidden-row"></td>
-				<td>
-				 <a href="/mobile/"><img src="/images/locations_small/msbhc.jpg" alt="Mobile Health Center"/></a>
-				 <p class="contact-location">Mobile Health Center</p>
-				 <p class="contact-info street">(765) 592-6164</p>
-				 <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
-				</td> 
-			</tr>
-		</table>
-		<div class="contact-questions">
-			<p class="contact-questions-title" class="vpchcblue">Have a question?</p>
-			<p class="contact-questions-content">Check out our FAQs section <a href="/faqs/">here</a>.</p>
-  	</div>
-  </div>
-</main>
-<footer><?php include("../php/includes/footer.php"); ?></footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="/scripts/vpchc_mobile_menu.js"></script>
-</body>
+<!doctype html>
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+<link href="/css/vpchc_styles.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128496723-1');
+</script>
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+</head>
+<body>
+<header><?php include("../php/includes/header.php"); ?></header>
+<main>
+  <div class="banner">
+    <img src="/images/banner/contact_us_index.jpg">
+    <div class="banner-title">
+      <h1>Contact Us</h1>
+    </div>
+  </div>
+  <div class="content centered medium">
+		<table class="contact-table">
+			<tr>
+				<td>
+				 <a href="/bloomingdale/"><img src="/images/locations_small/bloomingdale.jpg" alt="Bloomingdale"/></a>
+				 <p class="contact-location">Bloomingdale</p>
+				 <p class="contact-info street">201 W Academy Street</p>
+				 <p class="contact-info state">Bloomingdale, IN 47832</p>
+				 <p class="contact-info">(765) 498-9000</p>
+				</td>
+				<td>
+				 <a href="/cayuga/"><img src="/images/locations_small/cayuga.jpg" alt="Cayuga"/></a>
+				 <p class="contact-location">Cayuga</p>
+				 <p class="contact-info street">703 W Park Street</p>
+				 <p class="contact-info state">Cayuga, IN 47928</p>
+				 <p class="contact-info">(765) 498-9042</p>
+				</td> 
+				<td>
+				 <a href="/clinton/"><img src="/images/locations_small/clinton.jpg" alt="Clinton"/></a>
+				 <p class="contact-location">Clinton</p>
+				 <p class="contact-info street">777 S Main Street, #100</p>
+				 <p class="contact-info state">Clinton, IN 47842</p>
+				 <p class="contact-info">(765) 828-1003</p>
+				</td> 
+			</tr>
+			<tr>
+				<td>
+				 <a href="/crawfordsville/"><img src="/images/locations_small/banner_crawfordsville3.jpg" alt="Crawfordsville"/></a>
+				 <p class="contact-location">Crawfordsville</p>
+				 <p class="contact-info street">1702 Lafayette Road</p>
+				 <p class="contact-info state">Crawfordsville, IN 47933</p>
+				 <p class="contact-info">(765) 362-5100</p>
+				</td>
+				<td>
+				 <a href="/rockville/"><img src="/images/locations_small/rockville.jpg" alt="Rockville"/></a>
+				 <p class="contact-location">Rockville</p>
+				 <p class="contact-info street">727 Lincoln Rd</p>
+				 <p class="contact-info state">Rockville, IN 47842</p>
+				 <p class="contact-info">(765) 569-1123</p>
+				</td> 
+				<td>
+				 <a href="/terrehaute"><img src="/images/locations_small/terrehaute.jpg" alt="Terre Haute"/></a>
+				 <p class="contact-location">Terre Haute</p>
+				 <p class="contact-info street">1530 N 7th Street, #201</p>
+				 <p class="contact-info state">Terre Haute, IN 47807</p>
+				 <p class="contact-info">(812) 238-7631</p>
+				</td> 
+			</tr>
+			<tr>
+				<td class="contact-hidden-row"></td>
+				<td>
+				 <a href="/mobile/"><img src="/images/locations_small/msbhc.jpg" alt="Mobile Health Center"/></a>
+				 <p class="contact-location">Mobile Health Center</p>
+				 <p class="contact-info street">(765) 592-6164</p>
+				 <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+				</td> 
+			</tr>
+		</table>
+		<div class="contact-questions">
+			<p class="contact-questions-title" class="vpchcblue">Have a question?</p>
+			<p class="contact-questions-content">Check out our FAQs section <a href="faqs">here</a>.</p>
+  	</div>
+  </div>
+</main>
+<footer><?php include("../php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+</body>
 </html>
