69989ba55821f6028bdc54c739f6ea55b4bbdc3b
Elizabeth <elizabeth@itindianapolis.com>
2019-03-12 10:22:31 -0400

    Addition of new pages

---
+++ health-wellness/index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+<link href="/css/vpchc_styles.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128496723-1');
+</script>
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+</head>
+<body>
+<header><?php include("../php/includes/header.php"); ?></header>
+<main>
+	<div class="banner">
+    <img src="/images/banner/Health-and-wellness.jpg">
+    <div class="banner-title">
+      <h1>Health & Wellness</h1>
+    </div>
+  </div>
+  <div class="content">
+		<div class="diabetes-header">Exercise is an important part of your overall health!</div>
+		<br>
+    <p>Click one of the counties below to find available health and wellness opportunities nearest you</p>	<button class="accordion">Montgomery County</button>		<div class="panel">			<p>Sugar Creek Community Trail & Nature Park<br />			US 231 N/N Washington St, Crawfordsville, IN 47933<br />			765-364-5160			</p>			<p>Waynetown Aquatic Center/Splash Pad<br />			100 Harrington Dr, Waynetown, IN 47990<br />			765-234-2154			</p>			<p>Crawfordsville Parks & Recreation<br />			922 E. South Blvd, Crawfordsville, IN 47933<br />			765-364-5175			</p>			<p>Milligan Park & Pool/Splash Pad<br />			910 Mill St, Crawfordsville, IN 47933<br />			765-365-5174			</p>			<p>Crawfordsville High School Indoor Pool<br />			1 W. Athenian Dr, Crawfordsville, IN 47933<br />			765-364-3247			</p>			<p>North Montgomery High School<br />			5945 US 231, Crawfordsville, IN 47933<br />			765-362-5140			</p>		</div>	<button class="accordion">Parke County</button>		<div class="panel">			<p>Armory (Track)<br />			224 Strawberry Rd, Rockville, IN 47872<br />			317-247-3300			</p>			<p>Uptown Fitness<br />			114 S Market St, Rockville, IN 47872<br />			765-505-6348			</p>			<p>Elite Fitness<br />			120 S Market St, Rockville, IN 47872<br />			765-731-1123			</p>			<p>Rockville Lake (Frisbee Golf Course)<br />			828 Marshall Rd, Rockville, IN 47872<br />			765-569-6541			</p>			<p>Turkey Run State Park<br />			8121 Park Rd, Marshall, IN 47859<br />			765-597-2635			</p>			<p>Your Yoga Nest (chair exercises)<br />			215 E Ohio St, Rockville, IN 47872<br />			812-236-0336			</p>		</div>	<button class="accordion">Vermillion County</button>		<div class="panel">			<p>Athlete Industries<br />			111 S Main St, Clinton, IN 47842<br />			train@athleteindustries.com			</p>			<p>Cottrell’s Fitness Corner (24-hour facility)<br />			222 Vine St, Clinton, IN 47842<br />			765-832-6781			</p>			<p>Parke-Vermillion Christian Youth Mission<br />			115 S 5th St, Clinton, IN 47842<br />			765-505-5068			</p>			<p>Ripple Effect Fitness<br />			115 S 5th St, Clinton, IN 47842<br />			812-229-4257			</p>			<p>South Vermillion High School (track & pool)<br />			770 Wildcat Dr, Clinton, IN 47842<br />			765-832-3551			</p>			<p>Sportland Park (track)<br />			615 S 10th St, Clinton, IN 47842			</p>			<p>Vermillion County Fairgrounds<br />			325 W Maple St, Cayuga, IN 47928<br />			765-492-5330			</p>			<p>7th Street Health and Fitness and Tanning Salon<br />			1301 N 7th St, Clinton, IN 47842<br />			765-832-9844			</p>		</div>			<button class="accordion">Vigo County</button>		<div class="panel">			<p>Terre Haute YMCA<br />			951 Dresser Dr, Terre Haute, IN 47807<br />			812-232-8446			</p>			<p>Mount Pleasant Upward Basketball, Cheerleading, and Soccer (faith-based)<br />			3050 E. Davis Dr, Terre Haute, IN 47802<br />			812-232-4808			</p>			<p>Planet Fitness<br />			138 W Honey Creek Pkwy, Terre Haute, IN 47802<br />			812-235-5001			</p>			<p>Meadows Mall/Honey Creek Mall (mall walking)<br />			Meadows Mall: 2800 Poplar St, Terre Haute, IN 47803<br />			812-514-3216<br />			Honey Creek Mall: 3401 US-41, Terre Haute, In 47802<br />			812-232-4500			</p>			<p>Vigo County School Corporation Aquatic Center<br />			2230 Prairieton Rd, Terre Haute, IN 47802<br />			812-238-8381			</p>			<p>Yoga of 12 Step Recovery (addiction-focused)<br />			619 Washington Ave, Terre Haute, IN 47802<br />			812-917-5006			</p>		</div>
+		
+	</div>
+</main>
+<footer><?php include("../php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+</body>
+</html>
