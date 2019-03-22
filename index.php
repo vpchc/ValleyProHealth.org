20db2464e6bb8e109f42407ffc7049c8fd2d4829
Bill Oellig <boellig@uhhg.org>
2019-03-22 15:10:43 -0400

    Removed Louwanna Wallace
    Removed Louwanna Wallace from providers page and Bloomingdale page

--- index.php
+++ index.php
@@ -1,118 +1,112 @@
-<!doctype html>
-<!--
-    Valley Professionals Community Health Center Website 3.0
-    Developer - Brice Webster
-    Design    - Lindsay Gregg
--->
-<html lang="en">
-<head>
-<title>ValleyProHealth.org</title><link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
-<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="google-site-verification" content="3HI8hXSS6f73yfDQ7uLGY7ki6G0WPpDotkNuT_BfxnQ" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//valleyprohealth.org/stats/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-128496723-1');
-</script>
-
-<!--.....|.........|.........|.........|.........|.........|.........|.......-->
-	
-</head>
-<body>
-<header><?php include("php/includes/header.php"); ?></header>
-<main>
-	<div id="slideshow" class="fullscreen">
-		<div class="slide">
-			<img src="/images/homepage/slideshow/Patient Portal slide photo2.jpg">
-			<div class="slide-info fullscreen">
-				<p class="slide-title fullscreen">Your Health in Your Hands</p>
-				<p class="slide-subtitle fullscreen">Get control of your health by utilizing the new patient portal!</p>	
-				<a href="https://mycw108.ecwcloud.com/portal14763/jsp/100mp/login_otp.jsp" class="slide-info-button">Get Started<span class="slide-info-span"></span></a>
-			</div>
-		</div>
-		<div class="slide">
-			<img src="/images/homepage/slideshow/Rockville slide photo2.jpg">
-			<div class="slide-info fullscreen">
-				<p class="slide-title fullscreen">Our Rockville Clinic is Expanding</p>
-				<p class="slide-subtitle fullscreen">Learn more about our services!</p>
-				<a href="/rockville/" class="slide-info-button">Learn More<span class="slide-info-span"></span></a>
-			</div>
-		</div>
-		<div class="slide">
-			<img src="/images/homepage/slideshow/CHW slide photo2.jpg">
-			<div class="slide-info fullscreen">
-				<p class="slide-title fullscreen">Are you covered?</p>
-				<p class="slide-subtitle fullscreen">A Community Health Worker can help you find affordable health insurance.</p>
-				<a href="/services/patient_resources.php" class="slide-info-button">Learn More<span class="slide-info-span"></span></a>
-			</div>
-		</div>
-		<div class="slide">
-			<img src="/images/homepage/slideshow/Dental slide2.jpg">
-			<div class="slide-info fullscreen">
-				<p class="slide-title fullscreen">Dental services now in Crawfordsville!</p>
-				<p class="slide-subtitle fullscreen">Schedule an appointment today.</br></p>
-				<a href="/dental/" class="slide-info-button">Contact Us!<span class="slide-info-span"></span></a>
-			</div>
-		</div>
-	</div>
-	<div class="home-about">
-		<img class="home-about-hands" src="/images/services/ps_icon.png" alt="VPCHC Volunteer Example">
-        <p><a href="files/forms/en/VPCHC-%20Website%20Notice.docx.pdf">NOTICE OF DATA PRIVACY EVENT</br></br> </a>Valley Professionals Community Health Center is your home for health care!  As a Federally Qualified Health Center serving those in need and creating healthier communities, Valley Professionals provides primary care, behavioral health, dental and patient support services. </br></br>Valley Professionals has clinics located in Montgomery, Parke, Vermillion and Vigo counties in Indiana, as well as a Mobile School-Based Health Center that visits local schools providing primary and behavioral health services.</p>
-        <img class="home-about-info" src="/images/homepage/10years_thankyou.jpg"\>
-        </div>
-	<div class="home-button-row">
-		<div class="home-button"><a href="/careers/"><img src="/images/homepage/career_button.jpg"/></a>
-		</div>
-		<div class="home-button"><a href="locations/"><img src="/images/homepage/location_button.jpg"></a>
-		</div>
-		<div class="home-button"><a href="/about_us/news_&_events"><img src="/images/homepage/events_button.jpg"/></a></div>
-		<div class="home-button"><a href="/about_us/providers_&_team"><img src="/images/homepage/team_button.jpg"/></a>
-		</div>
-	</div>
-</main>
-<footer><div><?php include("php/includes/footer.php"); ?></footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="/scripts/vpchc_mobile_menu.js"></script>
-<script src="/scripts/jquery.bxslider.js"></script>
-<script src="/scripts/vpchc_slideshow.js"></script>
-<script src="/scripts/vpchc_screen_size_check.js"></script>
-</body>
+<!doctype html>
+<!--
+    Valley Professionals Community Health Center Website 3.0
+    Developer - Brice Webster
+    Design    - Lindsay Gregg
+-->
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="google-site-verification" content="3HI8hXSS6f73yfDQ7uLGY7ki6G0WPpDotkNuT_BfxnQ" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+
+
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+	
+</head>
+<body>
+<header><?php include("php/includes/header.php"); ?></header>
+<main>
+	<div id="slideshow" class="fullscreen">
+		<div class="slide">
+			<img src="/images/homepage/slideshow/Patient Portal slide photo2.jpg">
+			<div class="slide-info fullscreen">
+				<p class="slide-title fullscreen">Your Health in Your Hands</p>
+				<p class="slide-subtitle fullscreen">Get control of your health by utilizing the new patient portal!</p>	
+				<a href="https://mycw108.ecwcloud.com/portal14763/jsp/100mp/login_otp.jsp" class="slide-info-button">Get Started<span class="slide-info-span"></span></a>
+			</div>
+		</div>
+		<div class="slide">
+			<img src="/images/homepage/slideshow/Rockville slide photo2.jpg">
+			<div class="slide-info fullscreen">
+				<p class="slide-title fullscreen">Our Rockville Clinic is Expanding</p>
+				<p class="slide-subtitle fullscreen">Learn more about our services!</p>
+				<a href="/rockville/" class="slide-info-button">Learn More<span class="slide-info-span"></span></a>
+			</div>
+		</div>
+		<div class="slide">
+			<img src="/images/homepage/slideshow/CHW slide photo2.jpg">
+			<div class="slide-info fullscreen">
+				<p class="slide-title fullscreen">Are you covered?</p>
+				<p class="slide-subtitle fullscreen">A Community Health Worker can help you find affordable health insurance.</p>
+				<a href="/services/patient_resources.php" class="slide-info-button">Learn More<span class="slide-info-span"></span></a>
+			</div>
+		</div>
+		<div class="slide">
+			<img src="/images/homepage/slideshow/Dental slide2.jpg">
+			<div class="slide-info fullscreen">
+				<p class="slide-title fullscreen">Dental services now in Crawfordsville!</p>
+				<p class="slide-subtitle fullscreen">Schedule an appointment today.</br></p>
+				<a href="/dental/" class="slide-info-button">Contact Us!<span class="slide-info-span"></span></a>
+			</div>
+		</div>
+	</div>
+	<div class="home-about">
+		<img class="home-about-hands" src="/images/services/ps_icon.png" alt="VPCHC Volunteer Example">
+        <p><a href="files/forms/en/VPCHC-%20Website%20Notice.docx.pdf">NOTICE OF DATA PRIVACY EVENT</br></br> </a>Valley Professionals Community Health Center is your home for health care!  As a Federally Qualified Health Center serving those in need and creating healthier communities, Valley Professionals provides primary care, behavioral health, dental and patient support services. </br></br>Valley Professionals has clinics located in Montgomery, Parke, Vermillion and Vigo counties in Indiana, as well as a Mobile School-Based Health Center that visits local schools providing primary and behavioral health services.</p>
+        <img class="home-about-info" src="/images/homepage/10years_thankyou.jpg"\>
+        </div>
+	<div class="home-button-row">
+		<div class="home-button"><a href="/careers/"><img src="/images/homepage/career_button.jpg"/></a>
+		</div>
+		<div class="home-button"><a href="locations/"><img src="/images/homepage/location_button.jpg"></a>
+		</div>
+		<div class="home-button"><a href="/about_us/news_&_events"><img src="/images/homepage/events_button.jpg"/></a></div>
+		<div class="home-button"><a href="/about_us/providers_&_team"><img src="/images/homepage/team_button.jpg"/></a>
+		</div>
+	</div>
+</main>
+<footer><div><?php include("php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+<script src="/scripts/jquery.bxslider.js"></script>
+<script src="/scripts/vpchc_slideshow.js"></script>
+<script src="/scripts/vpchc_screen_size_check.js"></script>
+</body>
 </html>
