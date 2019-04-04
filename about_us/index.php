3d6a8259981bd9cd2ef549e90db74cf54a5c637b
Bill Oellig <boellig@uhhg.org>
2019-04-04 15:37:50 -0400

    Corrected Spelling About us
    Fixed two incorrectly spelled words on about us page welcome, and healthcare

--- about_us/index.php
+++ about_us/index.php
@@ -1,75 +1,75 @@
-<!doctype html>
-<html lang="en">
-<head>
-<title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//valleyprohealth.org/stats/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-128496723-1');
-</script>
-<!--.....|.........|.........|.........|.........|.........|.........|.......-->
-</head>
-<body>
-<header><?php include("../php/includes/header.php"); ?></header>
-<main>
-  <div class="banner">
-    <img src="/images/banner/about_us_index.jpg">
-    <div class="banner-title">
-      <h1>About Us</h1>
-    </div>
-  </div>
-  <div class="content">
-    <p>Valley Professionals Community Health Center is a Federally Qualified Health Center (FQHC) dedicated to serving those in need, a concept that is not exclusive to healthcare, but is present in everything we do. Whether a patient, fellow employee, local organization, or member of the community, we believe in rolling up our sleeves and lending a helping hand.</p><br />
-    <p>Valley Professionals opened its first clinic in Clinton, Indiana in 2008, and has since grown to include five additional clinics in Montgomery, Parke, Vermillion, and Vigo counties, as well as, a Mobile School-Based Health Center that visits fourteen schools in Parke and Vermillion counties.</p>
-    <h2>Mission</h2>
-		<p>Valley Professionals Community Health Center providers comprehensive and integrated health care for all individuals and families, while promoting health education opportunities for the community, students, and health care professionals.</p>
-    <h2>What is an FQHC?</h2>
-    <p>Federally Qualified Health Centers, or FQHCs, are non-profit private or public entities that serve medically underserved populations. Put simply, FQHCs exist to serve those who have limited access to healhtcare, financially or logistically. Although all individuals are welocme, FQHCs stand apart from private practices due to their ability to facilitate treatment for low income individuals, as well as, the uninsured or underinsured.</p>
-    <h2>Patient-Centered Medical Home</h2>
-    <p>Valley Professionals is your local medical home! Patient-Centered Medical Home(PCMH) is a new approach to your healthcare. PCMH provides all-inclusive primary care that creates partnerships between you and your family, and your primary care provider and health care team.</p>
-  </div>
-</main>
-<footer><?php include("../php/includes/footer.php"); ?></footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="/scripts/vpchc_mobile_menu.js"></script>
-</body>
+<!doctype html>
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128496723-1');
+</script>
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+</head>
+<body>
+<header><?php include("../php/includes/header.php"); ?></header>
+<main>
+  <div class="banner">
+    <img src="/images/banner/about_us_index.jpg">
+    <div class="banner-title">
+      <h1>About Us</h1>
+    </div>
+  </div>
+  <div class="content">
+    <p>Valley Professionals Community Health Center is a Federally Qualified Health Center (FQHC) dedicated to serving those in need, a concept that is not exclusive to healthcare, but is present in everything we do. Whether a patient, fellow employee, local organization, or member of the community, we believe in rolling up our sleeves and lending a helping hand.</p><br />
+    <p>Valley Professionals opened its first clinic in Clinton, Indiana in 2008, and has since grown to include five additional clinics in Montgomery, Parke, Vermillion, and Vigo counties, as well as, a Mobile School-Based Health Center that visits fourteen schools in Parke and Vermillion counties.</p>
+    <h2>Mission</h2>
+		<p>Valley Professionals Community Health Center providers comprehensive and integrated health care for all individuals and families, while promoting health education opportunities for the community, students, and health care professionals.</p>
+    <h2>What is an FQHC?</h2>
+    <p>Federally Qualified Health Centers, or FQHCs, are non-profit private or public entities that serve medically underserved populations. Put simply, FQHCs exist to serve those who have limited access to healthcare, financially or logistically. Although all individuals are welcome, FQHCs stand apart from private practices due to their ability to facilitate treatment for low income individuals, as well as, the uninsured or underinsured.</p>
+    <h2>Patient-Centered Medical Home</h2>
+    <p>Valley Professionals is your local medical home! Patient-Centered Medical Home(PCMH) is a new approach to your healthcare. PCMH provides all-inclusive primary care that creates partnerships between you and your family, and your primary care provider and health care team.</p>
+  </div>
+</main>
+<footer><?php include("../php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+</body>
 </html>
