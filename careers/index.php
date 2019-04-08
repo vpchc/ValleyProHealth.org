a16b7fabd67c9c4d5fbed58d505d67756108c778
Bill Oellig <boellig@uhhg.org>
2019-04-08 14:46:02 -0400

    Updated Careers page
    Added three job listings to careers page, Dental Office Coordinator, Call Center, NP

--- careers/index.php
+++ careers/index.php
@@ -131,6 +131,25 @@
 			<img src="/images/careers/location_pointer.png"/><p>Terre Haute</p>
       </div></a>
     </div>
+    <div>
+        <a href="/careers/job_listing_8.php"><div class="content-tile">
+			<p>Dental Office Coordinator</p>
+			<img src="/images/careers/location_pointer.png"/><p>Cayuga</p>
+      </div></a>
+    </div>
+    <div>
+        <a href="/careers/job_listing_9"><div class="content-tile">
+			<p>Call Center</p>
+			<img src="/images/careers/location_pointer.png"/><p>Clinton</p>
+     </div></a>
+    </div>
+    <div>
+        <a href="/careers/job_listing_10"><div class="content-tile">
+			<p>Nurse Practitioner</p>
+			<img src="/images/careers/location_pointer.png"/><p>Bloomingdale</p>
+     </div></a>
+    </div>
+    
          
 		<h2 id="benefits">Benefits</h2>
     <p>Valley Professionals offers excellent benefits, including:</p>
