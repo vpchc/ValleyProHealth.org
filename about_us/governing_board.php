e8c0627c1ade229fb78be0b0d29ea8e3004f8e57
Bill Oellig <boellig@uhhg.org>
2019-08-06 14:49:10 -0400

    Updated Board Members
    Removed Lisa Taylor

--- about_us/governing_board.php
+++ about_us/governing_board.php
@@ -1,146 +1,142 @@
-<!doctype html>
-<html lang="en">
-<head>
-<title>ValleyProHealth.org</title>
-<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
-<meta name="theme-color" content="#ffffff">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//valleyprohealth.org/stats/piwik/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-128496723-1');
-</script>
-<!--.....|.........|.........|.........|.........|.........|.........|.......-->
-</head>
-<body>
-<header><?php include("../php/includes/header.php"); ?></header>
-<main>
-	<div class="banner">
-    <img src="/images/banner/about_us_board.jpg">
-    <div class="banner-title">
-      <h1>Governing Board</h1>
-    </div>
-  </div>
-  <div class="content centered large">
-    <p>Valley Professionals Community Health Center has a governing board which consists of active and diverse community members that oversee the development of the mission, vision, and values of our clinic. These individuals are passionate about providing resources to the community and bringing healthcare services to all who are in need.</p>
-		<h3>Board Officers</h3>
-		<hr>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">John Beardsley<span>Board President</span></p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Mike Miller<span>Board Vice President</span></p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Doug Fauber<span>Board Secretary</span></p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Thomas Frantz<span>Board Treasurer</span></p>
-			</div>
-		</div>
-		<h3>Board Members</h3>
-		<hr>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Mary Helderman</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Diana Carole Hart</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Yale Yager</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Jackie Mathis</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Jeff Williamson</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Jerry Hawkins</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Dave Chapman</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Cindy Hein</p>
-			</div>
-		</div>
-		<div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Lou Bonomo</p>
-			</div>
-		</div>
-        <div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Greg Harbison</p>
-		    </div>
-	    </div>
-        <div class="tab">
-			<div class="tab-bar tabgrey">
-				<p class="tab-simple">Lisa Taylor</p>
-		    </div>
-	    </div>
-</main>
-<footer><?php include("../php/includes/footer.php"); ?></footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="/scripts/vpchc_mobile_menu.js"></script>
-</body>
+<!doctype html>
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128496723-1');
+</script>
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+</head>
+<body>
+<header><?php include("../php/includes/header.php"); ?></header>
+<main>
+	<div class="banner">
+    <img src="/images/banner/about_us_board.jpg">
+    <div class="banner-title">
+      <h1>Governing Board</h1>
+    </div>
+  </div>
+  <div class="content centered large">
+    <p>Valley Professionals Community Health Center has a governing board which consists of active and diverse community members that oversee the development of the mission, vision, and values of our clinic. These individuals are passionate about providing resources to the community and bringing healthcare services to all who are in need.</p>
+		<h3>Board Officers</h3>
+		<hr>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">John Beardsley<span>Board President</span></p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Mike Miller<span>Board Vice President</span></p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Doug Fauber<span>Board Secretary</span></p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Thomas Frantz<span>Board Treasurer</span></p>
+			</div>
+		</div>
+		<h3>Board Members</h3>
+		<hr>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Mary Helderman</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Diana Carole Hart</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Yale Yager</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Jackie Mathis</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Jeff Williamson</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Jerry Hawkins</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Dave Chapman</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Cindy Hein</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Lou Bonomo</p>
+			</div>
+		</div>
+        <div class="tab">
+			<div class="tab-bar tabgrey">
+				<p class="tab-simple">Greg Harbison</p>
+		    </div>
+	    </div>
+        
+</main>
+<footer><?php include("../php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+</body>
 </html>
