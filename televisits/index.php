2cdaee812a914dd907238b52ad94a980761fc4d4
Bill Oellig <boellig@uhhg.org>
2020-03-27 10:45:42 -0400

    Added Televisits Page
    Uploaded telemedicine FAQ page per IT Team and leadership

---
+++ televisits/index.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>ValleyProHealth.org</title>
+<link href="/css/vpchc_styles_3.0.css" rel="stylesheet" type="text/css" />
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//valleyprohealth.org/stats/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-128496723-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-128496723-1');
+</script>
+<!--.....|.........|.........|.........|.........|.........|.........|.......-->
+</head>
+<body>
+<header><?php include("../php/includes/header.php"); ?></header>
+<main>
+	<div class="banner">
+    <img src="/images/banner/healow2.jpg">
+    <div class="banner-title">
+  	<h1>Telemedicine FAQs</h1>
+    </div>
+  </div>
+  <div class="content centered medium">
+		
+		<h3 id="newpat">YOU WILL NEED THE PRACTICE CODE : HHJIAD</h3>
+		<hr>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">What are Televisits?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">A televisit is when your healthcare provider uses technology to care for you through a secure internet connection without you having to travel to their office. During your televisit, you and your healthcare provider will see and talk with each other using a computer or smart device (smartphone or computer). Your healthcare provider will be able to check how you’re doing, diagnose any problems you have, and come up with a treatment plan for you all by video. Your video won’t be recorded or saved.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">How do I start?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">You will need an email address to start. Give this to your provider’s office to help set you up.  From there you can use the Healow app (on your phone or tablet) or Healow.com on your laptop or desktop computer. Having trouble getting started? Click <a href="https://help.healow.com/faqs/how-do-i-log-in-for-the-first-time-18/">here</a> for steps on getting logged in for the first time.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">What is Healow?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">Healow is our patient portal which allows you to call into televisits, access your patient records, request refills, and communicate to with provider. Click <a href="https://www.youtube.com/watch?v=gZT_mLkNxgw&feature=youtu.be">here</a> to learn more information about Healow.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">What equipment do I need?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">Ideally, a smart phone or a laptop with video and audio capabilities are best.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">Does my insurance cover this?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">Most insurances cover this service, especially at this time (Spring 2020). If there are any issues with coverage, please contact our billing department.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">Who decides whether or not to provide services via televisits?</p>
+			</div>
+			<div class="tab-box">
+				<p class="tab-answer">The health care provider determines if a benefit or service is clinically appropriate to be provided, subject to consent by the patient.</p>
+			</div>
+		</div>
+		<div class="tab">
+			<div class="tab-bar blue">
+				<p class="tab-question">How do I contact my provider’s office?</p>
+			</div>
+			<div class="tab-box">
+			 <p class="tab-answer">To contact your provider’s office click <a href="https://valleyprohealth.org/contact_us/">here</a> to see the office’s phone number.</p>
+			</div>
+		</div>
+		
+	</div>
+</main>
+<footer><?php include("../php/includes/footer.php"); ?></footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="/scripts/vpchc_mobile_menu.js"></script>
+</body>
+</html>
